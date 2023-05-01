a1c4baf3de2d52c5e1e21b50e9fbd01eedf79801
Melisande <melisande.malonga@gmail.com>
2023-05-01 23:05:43 +0200

    Reorganize front folder

---
+++ FRONT_END/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      Hello world
+      <img src={logo}></img>
+    </div>
+  );
+}
+
+export default App;
