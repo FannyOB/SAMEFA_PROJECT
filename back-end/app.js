774511842a99f3c4da3472e7d7a4c194ffd2d630
Fanny <fanny.olengabinene@laposte.net>
2023-05-05 10:52:14 +0200

    restructure project

--- back-end/app.js
+++ back-end/app.js
@@ -4,6 +4,6 @@ const app = express()
 
 const port = 3000
 
-app.get('/', (req,res) => res.send('Hello, Express 2 ! '))
+app.get('/', (req,res) => res.send('Hello, Express 2!'))
 
 app.listen(port, ()  => console.log(`Notre application Node est démarée sur : http://localhost:${port}` ))
