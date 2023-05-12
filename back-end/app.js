10f08a2746306ed6fde06ce9abe01c15b9dc28ae
Sabrina Gooriah <sabrina.gooriah@qonto.com>
2023-05-12 16:16:53 +0200

    added eslint and jest

--- back-end/app.js
+++ back-end/app.js
@@ -1,9 +1,11 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
-const port = 3000
+const port = 3000;
 
-app.get('/', (req,res) => res.send('Hello, Express 2!'))
+app.get('/', (req, res) => res.send('Hello, Express 2!'));
 
-app.listen(port, ()  => console.log(`Notre application Node est démarée sur : http://localhost:${port}` ))
+app.listen(port, () =>
+  console.log(`Notre application Node est démarée sur : http://localhost:${port}`)
+);
