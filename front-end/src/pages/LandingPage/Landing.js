10618dd5188feaf8b88a50cbc0563f270af33a82
Sabrina Gooriah <sabrina.gooriah@qonto.com>
2023-05-24 12:03:41 +0200

    created pages and link between pages

---
+++ front-end/src/pages/LandingPage/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from '../../components/Card';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>New Page</h1>
+      <h1>Welcome to My Homepage</h1>
+      <p>This is a simple homepage component.</p>
+      <Card />
+    </div>
+  );
+};
+
+export default LandingPage;
