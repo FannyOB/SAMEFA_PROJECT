7ecc083c1e9d7f5c8e19ede072472a177f4db0cd
Fanny <fanny.olengabinene@laposte.net>
2023-05-25 12:05:49 +0200

    f

--- front-end/src/components/Card/Card.js
+++ front-end/src/components/Card/Card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./Card.css"
+import './Card.css'
 import { Link } from 'react-router-dom';
 import Ngo from '../../pages/Ngo';  
 
@@ -8,21 +8,19 @@ const Card = ({ title, description, image }) => {
   return (
     <div className="card">
       <Link to="/ngo">
-      <div className="card-content"> 
-        <h2 className="card-title">Card</h2>
-        <p className="card-description">{description}</p>
-      </div>
+        <div className="card-content">
+          <h2 className="card-title">Card</h2>
+          <p className="card-description">{description}</p>
+        </div>
       </Link>
-      
+
       <Link to="/ngo">
-      <div className="card-content">
-        <h2 className="card-title">Card</h2>
-        <p className="card-description">{description}</p>
-      </div>
+        <div className="card-content">
+          <h2 className="card-title">Card</h2>
+          <p className="card-description">{description}</p>
+        </div>
       </Link>
     </div>
-
-    
   );
 };
 
