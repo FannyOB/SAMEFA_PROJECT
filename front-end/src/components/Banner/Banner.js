76d077424b1239e8dce8c6cc542ac44a8fee681f
Melisande <melisande.malonga@gmail.com>
2023-07-06 11:05:01 +0200

    Banner design in progress: add HTML, start CSS

--- front-end/src/components/Banner/Banner.js
+++ front-end/src/components/Banner/Banner.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './Banner.css'
+import mainImg from '../../assets/main_img.png'
 
 const Banner = () => {
-    return (
-        <div>
-            <h1>Banner in progress</h1>
+    return (   
+        <div className='search-bar-wrapper'>
+            <div className='title-search-bar'>
+                <div className='title'>
+                    <h1>Boost</h1>
+                    <h1>your</h1>
+                    <h1>engagement</h1>
+                </div>
+                <div className='search-bar'>
+                    <input placeholder='Find your asso'/>
+                    <button>SEARCH</button>
+                </div>
+            </div>
+            <img src={mainImg} alt="banner img"/>
         </div>
     );
 };
--- front-end/src/pages/Homepage/Homepage.js
+++ front-end/src/pages/Homepage/Homepage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AssociationList from '../../components/AssociationList/AssociationList';
 import Header from '../../components/Header/Header';
+import Banner from '../../components/Banner/Banner';
 
 const Homepage = () => {
   return (
     <div>
       <Header/>
+      <Banner/>
       <AssociationList />
     </div>
   );
