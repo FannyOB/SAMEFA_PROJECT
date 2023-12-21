189edb13fa50091718e8ce98ef480ea57389a7b4
Fanny <fanny.olengabinene@laposte.net>
2023-12-21 15:31:27 +0100

    connecting_backend_and_frontend

--- back-end/app.js
+++ back-end/app.js
@@ -1,14 +1,15 @@
 import express from "express";
 import cors from "cors";
 
-//const express = require('express');
-
 const app = express();
 app.use(cors());
 
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello, Express 28!'));
+//app.get('/', (req, res) => res.send('Hello, Express 28!'));
+
+app.get('/getData', (req, res) => {res.send('connection ok!');
+});
 
 app.listen(port, () =>
   console.log(`Notre application Node est démarée sur : http://localhost:${port}`)
--- front-end/src/pages/Homepage/Homepage.jsx
+++ front-end/src/pages/Homepage/Homepage.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 import {AssociationList} from '../../components/Association';
 import Header from '../../components/Header/Header';
 import ComponentBanner from '../../components/ComponentBanner/ComponentBanner';
@@ -9,6 +10,19 @@ import './Homepage.scss'
 import Categories from '../../components/Categories/Categories';
 
 const Homepage = () => {
+
+  //connection front-end to back-end
+  const [data, setData]=useState("");
+
+  const getData=async()=>{
+    const response=await Axios.get("http://localhost:3000/getData");
+    setData(response.data);
+  }
+
+  useEffect(()=>{
+    getData()
+  }, []);
+
   return (
     <>
       <Header/>
@@ -19,6 +33,7 @@ const Homepage = () => {
       <SearchBar/>
       <AssociationList shouldSlice={false} className='asso-list' />
       <Categories/>
+      <div>{data}</div>
       <Footer/>
     </>
   );
