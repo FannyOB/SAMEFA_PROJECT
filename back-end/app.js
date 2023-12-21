f727dc79af51300ea828635419b994f2df1e7832
Fanny <fanny.olengabinene@laposte.net>
2023-12-21 14:37:44 +0100

    connecting in progress

--- back-end/app.js
+++ back-end/app.js
@@ -1,10 +1,14 @@
-const express = require('express');
+import express from "express";
+import cors from "cors";
+
+//const express = require('express');
 
 const app = express();
+app.use(cors());
 
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello, Express 2!'));
+app.get('/', (req, res) => res.send('Hello, Express 28!'));
 
 app.listen(port, () =>
   console.log(`Notre application Node est démarée sur : http://localhost:${port}`)
