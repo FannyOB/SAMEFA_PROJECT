af0b20bb9bd27625d4ac2a50c1086e9308620730
Fanny <fanny.olengabinene@laposte.net>
2024-02-25 20:25:02 +0100

    test to display TestAssoDatabase page

---
+++ front-end/src/pages/TestAssoDatabase.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../components/Header'
+
+
+const TestAssoDatabase = () => {
+
+    return (
+        <div>
+             <Header/>
+             
+           <h1>hello</h1>
+           
+        </div>
+    );
+};
+export default TestAssoDatabase;
+
