20035064530588215936e7dd1cded3a419e0eae8
Mlisande <melisande.malonga@gmail.com>
2024-03-07 15:59:57 +0100

    axios login

--- front-end/src/pages/LoginForm.jsx
+++ front-end/src/pages/LoginForm.jsx
@@ -1,19 +1,40 @@
 // composant Login, les lignes 13 et de 18 à 31 sont les appels avec supabase, à changer.
-
 import React, { useState } from 'react';
 import { Link, useNavigate  } from 'react-router-dom'; 
 import '../styles/pages/LoginForm.scss';
 import FormInput from '../components/FormInput';
-import logo from '../assets/img/logo_trans.png'
+import logo from '../assets/img/logo_trans.png';
+import axios from 'axios';
 
-const LoginForm = (setToken) => {
+const LoginForm = () => {
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
-
-  //const navigate = useNavigate();  // 2. Use useNavigation hook
+  const navigate = useNavigate();  // 2. Use useNavigation hook
 
   const handleSubmit = async (e) => {
-     e.preventDefault();
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3001/login', {
+        email: email,
+        password: pass,
+      });
+      if (response.status === 200) {
+        console.log(response.data.message);
+        navigate('/'); // Redirection vers la homepage uniquement si la connexion réussit
+      } else {
+        // Gérer les autres statuts de réponse (non-200)
+        console.error('Erreur lors de la connexion:', response.data.message);
+        if (response.status === 401) {
+          // Gérer le cas où le mot de passe est incorrect
+          console.error('Mot de passe incorrect:',response.data.message);
+        }
+      }
+    } catch (error) {
+      console.error('Erreur lors de la connexion:', error);
+      // En cas d'erreur, restez également sur la page de connexion
+    }
+  }
+  
     
   //  const { data, error } = await supabase.auth.signInWithPassword({
   //    email: email,
@@ -29,7 +50,7 @@ const LoginForm = (setToken) => {
   //    console.error("Login error:", error);
   //    // Handle the error appropriately, like showing an error message to the user
   //  }
-  }
+  
   return (
     <>
       <div className='container'>
